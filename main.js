59d4bd7bbc99d4e1d2613b43d18b5931cb1cb920
ericlebot <eriklebot@gmail.com>
2020-10-21 15:11:51 +0200

    added basic project files added resources

---
+++ main.js
@@ -0,0 +1,38 @@
+let assets;
+
+let game = hexi(512, 512, setup, assets, load);
+
+game.fps = 60;
+game.border = "1px black dashed";
+
+function play () {
+
+
+
+}
+
+function end () {
+
+
+
+}
+
+function setup () {
+
+
+
+}
+
+function reset ()  {
+
+
+
+}
+
+function load () {
+
+
+
+}
+
+game.start();
